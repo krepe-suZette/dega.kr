749bf2a16c75093ac60c0bea9f9007f356032595
krepe-suZette <37508580+krepe-suZette@users.noreply.github.com>
2021-04-25 03:23:53 +0900

    :bookmark: v0.5.6

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from parser_client import Client, Data
 app = Flask("main")
 p_client = Client(("localhost", 50001))
 data = Data()
-last_update = "2021-04-02"
-__version__ = "0.5.5"
+last_update = "2021-04-25"
+__version__ = "0.5.6"
 
 
 @app.route("/")
